d9e49553f8fd630c929d7ecd44f7d2c6f393b1b2
Aliaksei Karzhou <aliaksei_karzhou1@epam.com>
2022-07-22 22:21:57 +0300

    Task-3-2: Added separated users module & service

---
+++ src/users/users.service.ts
@@ -0,0 +1,46 @@
+import { Injectable } from '@nestjs/common';
+import { UserDto } from 'src/models/user-dto.interface';
+import { User } from 'src/models/user.interface';
+import { v4 as uuidv4 } from 'uuid';
+
+@Injectable()
+export class UsersService {
+  private users: User[] = [];
+
+  public createUser(userDto: UserDto): User {
+    const newUser: User = { id: uuidv4(), ...userDto };
+    this.users.push(newUser);
+    return newUser;
+  }
+
+  public getAllUsers(loginSubstring = '', limit = 10): User[] {
+    return this.users
+      .filter(
+        (user: User): boolean =>
+          user.login.includes(loginSubstring) && !user.isDeleted,
+      )
+      .sort((a: User, b: User) => (a.login > b.login ? 1 : -1))
+      .slice(0, limit);
+  }
+
+  public getUserById(id: string): User {
+    return this.users.find(
+      (user: User): boolean => user.id === id && !user.isDeleted,
+    );
+  }
+
+  public updateUser(id: string, userDto: UserDto): User {
+    this.users = this.users.map(
+      (user: User): User => (user.id === id ? { id, ...userDto } : user),
+    );
+    return this.getUserById(id);
+  }
+
+  public deleteUser(id: string): User {
+    this.users = this.users.map(
+      (user: User): User =>
+        user.id === id ? { ...user, isDeleted: true } : user,
+    );
+    return this.getUserById(id);
+  }
+}

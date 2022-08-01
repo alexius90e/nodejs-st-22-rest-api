3ed3210b51b48c52a009a61bc03a769f43ece8f1
Aliaksei Karzhou <aliaksei_karzhou1@epam.com>
2022-08-01 13:35:42 +0300

    Task-4-2: Implemented using SequelizeModule in AppModule

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,28 @@
 import { Module } from '@nestjs/common';
 import { UsersModule } from './users/users.module';
-import { DatabaseModule } from './database/database.module';
 import { ConfigModule } from '@nestjs/config';
+import { SequelizeModule } from '@nestjs/sequelize';
+import { User } from './models/user.entity';
 
 @Module({
-  imports: [ConfigModule.forRoot(), DatabaseModule, UsersModule],
+  imports: [
+    ConfigModule.forRoot(),
+    SequelizeModule.forRoot({
+      dialect: 'postgres',
+      host: process.env.POSTGRESDB_HOST,
+      port: Number(process.env.POSTGRESDB_PORT),
+      username: process.env.POSTGRESDB_USER,
+      password: process.env.POSTGRESDB_PASSWORD,
+      database: process.env.POSTGRESDB_DATABASE,
+      models: [User],
+      dialectOptions: {
+        ssl: {
+          require: true,
+          rejectUnauthorized: false,
+        },
+      },
+    }),
+    UsersModule,
+  ],
 })
 export class AppModule {}

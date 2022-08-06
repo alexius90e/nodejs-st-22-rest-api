ae52e21693f388b627bd173b32c2cd7fcc95d966
Aliaksei Karzhou <aliaksei_karzhou1@epam.com>
2022-08-06 23:50:01 +0300

    Task-5-1: Created group models, classes & types

--- src/groups/dto/create-group.dto.ts
+++ src/groups/dto/create-group.dto.ts
@@ -1,1 +1,11 @@
-export class CreateGroupDto {}
+import { IsArray, IsNotEmpty, IsString } from 'class-validator';
+import { Permission } from '../entities/permission.entity';
+
+export class CreateGroupDto {
+  @IsString()
+  @IsNotEmpty()
+  public name: string;
+
+  @IsArray()
+  public permissions: Permission[];
+}
--- src/groups/entities/group.entity.ts
+++ src/groups/entities/group.entity.ts
@@ -1,1 +1,20 @@
-export class Group {}
+import {
+  Column,
+  IsArray,
+  Model,
+  PrimaryKey,
+  Table,
+} from 'sequelize-typescript';
+import { Permission } from './permission.entity';
+
+@Table
+export class Group extends Model {
+  @PrimaryKey
+  @Column
+  id: string;
+
+  @Column({ unique: true })
+  name: string;
+
+  permissions: Permission[];
+}

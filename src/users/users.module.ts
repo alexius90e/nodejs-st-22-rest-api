c841d1fed63082c3bbf5cdc930a4e6135eede13e
Aliaksei Karzhou <aliaksei_karzhou1@epam.com>
2022-08-09 12:47:22 +0300

    Task-5-2: Renamed user model file

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -1,6 +1,6 @@
 import { Module } from '@nestjs/common';
 import { SequelizeModule } from '@nestjs/sequelize';
-import { User } from 'src/users/entities/user.entity';
+import { User } from 'src/users/models/user.model';
 import { UsersController } from './users.controller';
 import { UsersService } from './users.service';
 

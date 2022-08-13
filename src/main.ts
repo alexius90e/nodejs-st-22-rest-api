aa40b0d623174af5b8d8650fd4c3f266dedc6ab9
Aliaksei Karzhou <aliaksei_karzhou1@epam.com>
2022-08-14 00:07:23 +0300

    Task-6-2: Added logger at LoggerMiddleware

--- src/main.ts
+++ src/main.ts
@@ -2,14 +2,6 @@ import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
-process
-  .on('uncaughtException', (err, origin) => {
-    console.log(`Caught exception: ${err}\nException origin: ${origin}`);
-  })
-  .on('unhandledRejection', (reason, promise) => {
-    console.error('Unhandled Rejection at:', promise, 'reason:', reason);
-  });
-
 async function bootstrap(): Promise<void> {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe());

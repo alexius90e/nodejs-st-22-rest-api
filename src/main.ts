660a33640a9dc89676e3d03d1a7252fc8788237f
Aliaksei Karzhou <aliaksei_karzhou1@epam.com>
2022-08-14 20:15:48 +0300

    Task-6-2: Added filter for all errors & exceptions

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,12 @@
 import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { AllExceptionsFilter } from './middlewares/all-exceptions.filter';
 
 async function bootstrap(): Promise<void> {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe());
+  app.useGlobalFilters(new AllExceptionsFilter());
   await app.listen(3000);
 }
 bootstrap();

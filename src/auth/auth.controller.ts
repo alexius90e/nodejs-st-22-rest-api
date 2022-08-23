b612a0dec183c3abf6fe5c385be5dabab63b3caa
Aliaksei Karzhou <aliaksei_karzhou1@epam.com>
2022-08-24 00:02:35 +0300

    Task-7-1:  Added jwt functionality

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,12 +1,15 @@
 import { Controller, Post, Request, UseGuards } from '@nestjs/common';
-import { User } from 'src/users/models/user.model';
+import { AuthService } from './auth.service';
 import { LocalAuthGuard } from './guards/local-auth.guard';
+import { AccessToken } from './interfaces/access-token.interface';
 
 @Controller('v1/auth')
 export class AuthController {
+  constructor(private authService: AuthService) {}
+
   @UseGuards(LocalAuthGuard)
   @Post('login')
-  async login(@Request() req): Promise<User> {
-    return req.user;
+  async login(@Request() req): Promise<AccessToken> {
+    return this.authService.login(req.user);
   }
 }
--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -4,9 +4,17 @@ import { UsersModule } from 'src/users/users.module';
 import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
 import { LocalStrategy } from './guards/local.strategy';
+import { JwtModule } from '@nestjs/jwt';
 
 @Module({
-  imports: [UsersModule, PassportModule],
+  imports: [
+    UsersModule,
+    PassportModule,
+    JwtModule.register({
+      secret: String(process.env.JWT_SECRET),
+      signOptions: { expiresIn: '60s' },
+    }),
+  ],
   providers: [AuthService, LocalStrategy],
   controllers: [AuthController],
 })

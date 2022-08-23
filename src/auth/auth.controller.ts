4f6fe208c6e0e6a2b31347ce4a3b37edf788436f
Aliaksei Karzhou <aliaksei_karzhou1@epam.com>
2022-08-23 23:12:08 +0300

    Task-7-1:  Implemented passport local

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,8 +1,10 @@
-import { Controller, Post, Request } from '@nestjs/common';
+import { Controller, Post, Request, UseGuards } from '@nestjs/common';
 import { User } from 'src/users/models/user.model';
+import { LocalAuthGuard } from './guards/local-auth.guard';
 
 @Controller('v1/auth')
 export class AuthController {
+  @UseGuards(LocalAuthGuard)
   @Post('login')
   async login(@Request() req): Promise<User> {
     return req.user;
--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,11 +1,13 @@
 import { Module } from '@nestjs/common';
+import { PassportModule } from '@nestjs/passport';
 import { UsersModule } from 'src/users/users.module';
 import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
+import { LocalStrategy } from './guards/local.strategy';
 
 @Module({
-  imports: [UsersModule],
-  providers: [AuthService],
+  imports: [UsersModule, PassportModule],
+  providers: [AuthService, LocalStrategy],
   controllers: [AuthController],
 })
 export class AuthModule {}

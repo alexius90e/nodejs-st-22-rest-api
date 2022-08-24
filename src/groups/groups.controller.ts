205a426123ff81a2df9599841f87d8242c66aa84
Aliaksei Karzhou <aliaksei_karzhou1@epam.com>
2022-08-24 12:03:39 +0300

    Task-7-1: Implemented JwtAuthGuard at GroupsController

--- src/groups/groups.controller.ts
+++ src/groups/groups.controller.ts
@@ -8,31 +8,37 @@ import {
   Put,
   HttpException,
   HttpStatus,
+  UseGuards,
 } from '@nestjs/common';
 import { GroupsService } from './groups.service';
 import { CreateGroupDto } from './dto/create-group.dto';
 import { UpdateGroupDto } from './dto/update-group.dto';
 import { Group } from './models/group.model';
+import { JwtAuthGuard } from 'src/auth/guards/jwt-auth.guard';
 
 @Controller('v1/groups')
 export class GroupsController {
   constructor(private readonly groupsService: GroupsService) {}
 
+  @UseGuards(JwtAuthGuard)
   @Post()
   public async createGroup(@Body() createGroupDto: CreateGroupDto): Promise<Group> {
     return this.groupsService.create(createGroupDto);
   }
 
+  @UseGuards(JwtAuthGuard)
   @Get()
   public async findAllGroups(): Promise<Group[]> {
     return this.groupsService.findAll();
   }
 
+  @UseGuards(JwtAuthGuard)
   @Get(':id')
   public async findOneGroup(@Param('id') id: string): Promise<Group> {
     return this.groupsService.findOne(id);
   }
 
+  @UseGuards(JwtAuthGuard)
   @Put(':id')
   public async updateGroup(
     @Param('id') id: string,
@@ -41,11 +47,13 @@ export class GroupsController {
     return this.groupsService.update(id, updateGroupDto);
   }
 
+  @UseGuards(JwtAuthGuard)
   @Delete(':id')
   public async removeGroup(@Param('id') id: string): Promise<number> {
     return this.groupsService.remove(id);
   }
 
+  @UseGuards(JwtAuthGuard)
   @Post(':id')
   public async addUsersToGroup(
     @Param('id') groupId: string,

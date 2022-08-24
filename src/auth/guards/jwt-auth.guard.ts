ea56151c8957f2afc0296e635bbd5526dbbeb12f
Aliaksei Karzhou <aliaksei_karzhou1@epam.com>
2022-08-24 13:21:45 +0300

    Task-7-1: Refactored JwtAuthGuard

--- src/auth/guards/jwt-auth.guard.ts
+++ src/auth/guards/jwt-auth.guard.ts
@@ -1,5 +1,12 @@
-import { Injectable } from '@nestjs/common';
+import { ForbiddenException, Injectable, UnauthorizedException } from '@nestjs/common';
 import { AuthGuard } from '@nestjs/passport';
+import { User } from 'src/users/models/user.model';
 
 @Injectable()
-export class JwtAuthGuard extends AuthGuard('jwt') {}
+export class JwtAuthGuard extends AuthGuard('jwt') {
+  handleRequest<TUser = Partial<User>>(err: Error, user: TUser, info: any): TUser {
+    if (info && info.message === 'invalid signature') throw new ForbiddenException();
+    if (err || !user) throw err || new UnauthorizedException();
+    return user;
+  }
+}

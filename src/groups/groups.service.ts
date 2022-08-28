78ae560e495d5da170bacded7179383f979d93f3
Aliaksei Karzhou <aliaksei_karzhou1@epam.com>
2022-08-29 00:05:27 +0300

    Task-8-1: Added separated groups database api

--- src/groups/groups.service.ts
+++ src/groups/groups.service.ts
@@ -1,55 +1,35 @@
 import { Injectable } from '@nestjs/common';
-import { InjectModel } from '@nestjs/sequelize';
-import { Sequelize } from 'sequelize-typescript';
 import { CreateGroupDto } from './dto/create-group.dto';
 import { UpdateGroupDto } from './dto/update-group.dto';
 import { Group } from './models/group.model';
-import { User } from 'src/users/models/user.model';
+import { GroupsRepository } from './groups.repository';
 
 @Injectable()
 export class GroupsService {
-  constructor(
-    @InjectModel(Group) private groupsRepository: typeof Group,
-    @InjectModel(User) private usersRepository: typeof User,
-    private sequelize: Sequelize,
-  ) {}
+  constructor(private groupsRepository: GroupsRepository) {}
 
   public async create(createGroupDto: CreateGroupDto): Promise<Group> {
     return this.groupsRepository.create({ ...createGroupDto });
   }
 
   public async findAll(): Promise<Group[]> {
-    return this.groupsRepository.findAll<Group>();
+    return this.groupsRepository.findAll();
   }
 
   public async findOne(id: string): Promise<Group> {
-    return this.groupsRepository.findOne<Group>({ where: { id } });
+    return this.groupsRepository.findOne(id);
   }
 
   public async update(id: string, updateGroupDto: UpdateGroupDto): Promise<Group> {
-    await this.groupsRepository.update({ ...updateGroupDto }, { where: { id } });
+    await this.groupsRepository.update(id, updateGroupDto);
     return this.findOne(id);
   }
 
   public async remove(id: string): Promise<number> {
-    return this.groupsRepository.destroy({ where: { id } });
+    return this.groupsRepository.destroy(id);
   }
 
   public async addUsersToGroup(groupId: string, userIds: string[]): Promise<void> {
-    try {
-      await this.sequelize.transaction(async (transaction) => {
-        const group = await this.groupsRepository.findOne({
-          where: { id: groupId },
-          transaction,
-        });
-        const users = await Promise.all(
-          userIds.map((id) => this.usersRepository.findOne({ where: { id } })),
-        );
-
-        await group.$add('users', users, { transaction });
-      });
-    } catch (error: unknown) {
-      throw error;
-    }
+    this.groupsRepository.addUsersToGroup(groupId, userIds);
   }
 }

82f32148d08bb4374f84a2bd401e942a414e37f1
Aliaksei Karzhou <aliaksei_karzhou1@epam.com>
2022-08-28 22:42:49 +0300

    Task-8-1: Added separated users database api

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,18 +1,18 @@
 import { Injectable } from '@nestjs/common';
-import { InjectModel } from '@nestjs/sequelize';
 import { UserDto } from 'src/users/dto/user.dto';
 import { User } from 'src/users/models/user.model';
+import { UsersRepository } from './users.repository';
 
 @Injectable()
 export class UsersService {
-  constructor(@InjectModel(User) private usersRepository: typeof User) {}
+  constructor(private usersRepository: UsersRepository) {}
 
   public async createUser(userDto: UserDto): Promise<User> {
-    return this.usersRepository.create({ ...userDto });
+    return this.usersRepository.create(userDto);
   }
 
   public async getAllUsers(substring = '', limit = 10): Promise<User[]> {
-    return this.usersRepository.findAll<User>().then((users): User[] =>
+    return this.usersRepository.findAll().then((users): User[] =>
       users
         .filter((user: User): boolean => user.login.includes(substring))
         .sort((a: User, b: User): number => (a.login > b.login ? 1 : -1))
@@ -21,19 +21,19 @@ export class UsersService {
   }
 
   public async getUserById(id: string): Promise<User> {
-    return this.usersRepository.findOne({ where: { id } });
+    return this.usersRepository.findOneById(id);
   }
 
   public async getUserByLogin(login: string): Promise<User> {
-    return this.usersRepository.findOne({ where: { login } });
+    return this.usersRepository.findOneByLogin(login);
   }
 
   public async updateUser(id: string, userDto: UserDto): Promise<User> {
-    await this.usersRepository.update({ id, ...userDto }, { where: { id }, returning: true });
+    await this.usersRepository.update(id, userDto);
     return this.getUserById(id);
   }
 
   public async deleteUser(id: string): Promise<number> {
-    return this.usersRepository.destroy({ where: { id } });
+    return this.usersRepository.destroy(id);
   }
 }
